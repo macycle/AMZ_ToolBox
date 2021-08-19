cf0be65e5f94a04fefad218c114e1a8c99ac1d67
unknown <1124942097@qq.com>
2021-08-19 22:58:54 +0800

    初始化

---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{Make()}}</h3>
+  </div>
+</template>
+
+<script>
+import Cardata from "../assets/Cardata.json";
+export default {
+  data() {
+    return {
+    };
+  },
+  methods:{
+      Make(){
+          for(let key in Cardata){
+              console.log(key);
+          }
+      }
+  }
+ 
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- <Sidebar></Sidebar>
+    <div class="main-container">
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <AppMain></AppMain>
+    </div> -->
+    <Search />
+  </div>
+</template>
+
+<script>
+import AppMain from "../components/AppMain.vue";
+import Navbar from "../components/Navbar.vue";
+import Sidebar from "../components/Sidebar.vue";
+import Search from '../components/Search.vue'
+export default {
+  name: "Layout",
+  components: {
+    Navbar,
+    Sidebar,
+    AppMain,
+    Search
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
